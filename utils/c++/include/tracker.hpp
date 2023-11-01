8dd73969b820ff0f74202598fce4d580db91a6f3
sagi <sagi21805@gmail.com>
2023-11-01 19:09:23 +0200

    NEW: Created tracker class

---
+++ utils/c++/include/tracker.hpp
@@ -0,0 +1,18 @@
+#ifndef __TRACKER__HPP
+#define __TRACKER_HPP
+
+class Tracker{
+
+    private:
+        int **pointsCurrent;
+        int **pointsPrev;
+
+    public:
+        Tracker();
+        
+
+
+};
+
+
+#endif
